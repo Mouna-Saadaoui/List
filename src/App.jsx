6d55b4b2ede427871abd6d6c380456c09a8f88b1
Mouna Saadawi <saadaouimouna@tbs.u-tunis.tn>
2024-04-08 00:10:30 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
- 
- /*Display a js object */ 
- import * as React from "react"; 
- const welcome= { 
-  name :" Mouna",  
-  age : 20,
+import * as React from "react"; 
 
- }; 
- function App(){  
-  return( 
-    <h1>Hello, I'm {welcome.name}, I'm {welcome.age}</h1>
-  )
+const Search = ({ setSearchTerm }) => { 
+  const handleChange = (event) => { 
+    setSearchTerm(event.target.value);
+  } 
+  return ( 
+    <div> 
+      <label>Search for the games you played : </label>
+      <input type="text" onChange={handleChange}/>
+    </div>
+  );
+};
 
- } 
- export default App;
- 
+function App() {   
+  const [games] = React.useState([ 
+    { id: 1, name: "The sims 3" }, 
+    { id: 2, name: "The sims 2" }, 
+    { id: 3, name: "The sims 4" }, 
+    { id: 4, name: "MySims" }, 
+    { id: 5, name: "A Date with Death" }, 
+    { id: 6, name: "Gta 4" }, 
+    { id: 7, name: "Gta San Andreas" }, 
+    { id: 8, name: "Gta Vice City" }, 
+    { id: 9, name: "Coffee Talk" }, 
+    { id: 10, name: "Minecraft" }, 
+    { id: 11, name: "Until Then" }, 
+    { id: 12, name: "Good Pizza , Great Pizaa" },
+  ]); 
+  
+  const [searchTerm, setSearchTerm] = React.useState(''); 
+  const searchedGames = games.filter(function(game) { 
+    return game.name.toLowerCase().includes(searchTerm.toLowerCase()); 
+  }); 
+  
+  return ( 
+    <div> 
+      <ul> 
+        {searchTerm && searchedGames.map(function(game) { 
+          return <li key={game.id}>{game.name}</li>; 
+        })} 
+      </ul> 
+      <Search setSearchTerm={setSearchTerm} />
+    </div> 
+  ); 
+} 
+
+export default App;
